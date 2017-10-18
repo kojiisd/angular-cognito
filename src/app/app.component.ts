ecf0bf29c0f83f97224855c95c4fe895335eff66
Koji Ishida <kishida58@gmail.com>
2017-10-18 19:40:49 +0630

    Design and Amazon Cognito combination implemented.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { CognitoService } from './cognito.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [CognitoService]
 })
 export class AppComponent {
-  title = 'app';
+  userName = '';
+  password = '';
+
+  constructor(
+    private cognitoService: CognitoService,
+    private http: Http
+  ) { }
+
+  signIn() {
+    this.cognitoService.signIn(this.userName, this.password);
+  }
 }
